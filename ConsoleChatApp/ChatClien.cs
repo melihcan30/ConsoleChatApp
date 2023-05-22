4ae5ca9def75c73d4e3460b1dd31998eff7ba578
Melihcan Serbest <canmelih.free3@gmail.com>
2023-05-22 18:18:31 +0300

    Add project files.

---
+++ ConsoleChatApp/ChatClien.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+public class ChatClient
+{
+	private HubConnection _connection;
+
+	public async Task StartAsync()
+	{
+		_connection = new HubConnectionBuilder()
+			.WithUrl("https://localhost:5001")
+			.Build();
+
+		_connection.On<string>("ReceiveMessage", (message) =>
+		{
+			Console.WriteLine("New message: " + message);
+		});
+
+		await _connection.StartAsync();
+		Console.WriteLine("Connected to the server.");
+
+		while (true)
+		{
+			var input = Console.ReadLine();
+			await _connection.InvokeAsync("SendMessage", input);
+		}
+	}
+
+	public async Task StopAsync()
+	{
+		await _connection.StopAsync();
+		await _connection.DisposeAsync();
+	}
+}
+
---
+++ ConsoleChatApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleChatApp
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var chatClient = new ChatClient();
+			await chatClient.StartAsync();
+
+			Console.WriteLine("Press Enter to exit.");
+			Console.ReadLine();
+
+			await chatClient.StopAsync();
+		}
+	}
+}
+
